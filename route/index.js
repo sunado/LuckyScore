9ecd5c877076cd0b6b18fb0cd1cf984fee058641
sunado <quangdv1996@gmail.com>
2018-07-18 04:30:47 +0000

    init branch

--- route/index.js
+++ route/index.js
@@ -1,14 +1,18 @@
-'use strict'
-var express = require('express')
+'use strict';
+var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req,res,next){
-    res.render('index')
-})
+    res.render('index');
+});
 
 
 router.get('/login', function(req,res,next){
-    res.render('login')
-})
+    res.render('login');
+});
+
+router.get('/forgot-pw', function(req,res,next){
+    res.render('forgot-password');
+});
 
 module.exports = router;
