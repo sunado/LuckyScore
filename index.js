63c0db75f6ea5a19a81c462318d7a08216466458
sunado <quangdv1996@gmail.com>
2018-08-14 22:36:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -30,5 +30,6 @@ app.use(body_parser.urlencoded({extended: false}))
 
 app.use('/',route)
 
-app.listen(8080, () => console.log("App run on port 8080"))
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log("App run on port " + port))
 
